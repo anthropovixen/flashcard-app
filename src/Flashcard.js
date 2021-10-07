512e8fda052dd7e8bc9ade3ceee1076eaa0da66b
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-10-07 20:37:50 +1100

    Update Flashcard.js
    Create variable to have the card flipping.
    onClick inside div.

--- src/Flashcard.js
+++ src/Flashcard.js
@@ -1,5 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Flashcard() {
-	return <div></div>;
+export default function Flashcard({ flashcard }) {
+	/**  variables to have the card flipping. set state to false to start showing the front side*/
+	const [flip, setFlip] = useState(false);
+
+	/**set onClick inside div as a function, everytime we click here we set the current flip to non flip.
+	 * Toggling the current flip to the other option.
+	 */
+	return (
+		<div onClick={() => setFlip(!flip)}>
+			{/** if flip is true, we should return the answer, otherwise we should return the question*/}
+			{flip ? flashcard.answer : flashcard.question}
+		</div>
+	);
 }
