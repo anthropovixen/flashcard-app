522f06c3bb7ee289828665294a0dfb44fd941445
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-10-13 21:23:58 +1100

    Update App.js
    Import useRef
    Remove sample data and initialise state with empty arrays for flashcards and categories of questions
    Connect with api
    Function to decode html text
    Function to handle submits on forms and prevent default from happening
    Amount of questions and categories available from connection with API
    Create header form with categories, amount of questions and generate button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import FlashcardList from './FlashcardList';
 import './app.css';
 import axios from 'axios';
 
 function App() {
-	const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
+	const [flashcards, setFlashcards] = useState([]);
+	const [categories, setCategories] = useState([]);
+
+	const categoryEl = useRef();
+	const amountEl = useRef();
 
 	useEffect(() => {
+		axios.get('https://opentdb.com/api_category.php').then((res) => {
+			setCategories(res.data.trivia_categories);
+		});
+	}, []);
+
+	useEffect(() => {}, []);
+
+	function decodeString(str) {
+		const textArea = document.createElement('textarea');
+		textArea.innerHTML = str;
+		return textArea.value;
+	}
+
+	function handleSubmit(e) {
+		e.preventDefault();
 		axios
-			.get('https://opentdb.com/api.php?amount=20&category=18')
+			.get('https://opentdb.com/api.php', {
+				params: {
+					amount: amountEl.current.value,
+					category: categoryEl.current.value,
+				},
+			})
 			.then((res) => {
 				setFlashcards(
 					res.data.results.map((questionItem, index) => {
@@ -27,43 +51,43 @@ function App() {
 				);
 				console.log(res.data);
 			});
-	}, []);
-
-	function decodeString(str) {
-		const textArea = document.createElement('textarea');
-		textArea.innerHTML = str;
-		return textArea.value;
 	}
 
 	return (
-		<div className="container">
-			<FlashcardList flashcards={flashcards} />
-		</div>
+		<>
+			<form className="header" onSubmit={handleSubmit}>
+				<div className="form-group">
+					<label htmlFor="category">Category</label>
+					<select id="category" ref={categoryEl}>
+						{categories.map((category) => {
+							return (
+								<option value={category.id} key={category.id}>
+									{category.name}
+								</option>
+							);
+						})}
+					</select>
+				</div>
+				<div className="form-group">
+					<label htmlFor="amount">Number Of Questions</label>
+					<input
+						type="number"
+						id="amount"
+						min="1"
+						step="1"
+						defaultValue={10}
+						ref={amountEl}
+					/>
+				</div>
+				<div className="form-group">
+					<button className="btn">Generate</button>
+				</div>
+			</form>
+			<div className="container">
+				<FlashcardList flashcards={flashcards} />
+			</div>
+		</>
 	);
 }
-
-const SAMPLE_FLASHCARDS = [
-	{
-		id: 1,
-		question: 'Describe componentDidMount()',
-		answer:
-			'invoked after a component is mounted(inserted into the three). Initialization that requires DOM nodes should go here. Instantiate network request to load data from a remote endpoint.',
-		options: ['1', '2', '3'],
-	},
-	{
-		id: 2,
-		question: 'What is React?',
-		answer:
-			'An open-source frontend JavaScript library which is used fo building user interfaces for single page applications. Handles view layer for web and mobile apps. Created by Jordan Walke, a engineer working for FB. First deployed in 2011',
-		options: ['1', '2', '3'],
-	},
-	{
-		id: 3,
-		question: 'What are the major features of React?',
-		answer:
-			'Uses VirtualDOM instead RealDOM - RealDOM manipulations are expensive. Supports server-side rendering. Follows unidrectional data flow or binding. Uses reusable/composable UI components to develop the view',
-		options: ['1', '2', '3'],
-	},
-];
 
 export default App;
